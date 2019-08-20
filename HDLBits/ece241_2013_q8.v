b2629756b585744fe173cae837ac1b239e80b760
Explainaur <843347859@qq.com>
2019-08-20 15:13:33 +0800

    add solution

--- HDLBits/ece241_2013_q8.v
+++ HDLBits/ece241_2013_q8.v
@@ -15,8 +15,6 @@ module top_module (
             {one, 1'b1}:	next_state = one;
             {two, 1'b0}:	next_state = idle;
             {two, 1'b1}:	next_state = three;
-            {three, 1'b0}:	next_state = two;
-            {three, 1'b1}:	next_state = one;
         endcase
     end
     
@@ -31,43 +29,55 @@ module top_module (
     
 endmodule
 
+/* 官方
+*
+    * module top_module (
+        input clk,
+        input aresetn,
+        input x,
+        output reg z
+);
+
+        // Give state names and assignments. I'm lazy, so I like to use decimal numbers.
+        // It doesn't really matter what assignment is used, as long as they're unique.
+        parameter S=0, S1=1, S10=2;
+        reg[1:0] state, next;           // Make sure state and next are big enough to hold the state encodings.
 
 
 
+        // Edge-triggered always block (DFFs) for state flip-flops. Asynchronous reset.
+        always@(posedge clk, negedge aresetn)
+                if (<= S;
+                else
+                        state <= next;
 
-/*
-module top_module (
-    input clk,
-    input aresetn,    // Asynchronous active-low reset
-    input x,
-    output z ); 
-    reg [1:0] status,next_status;
-    parameter READY=0, FIRST=1, SECOND=2;
-    always @(*) begin
-        case (status)
-            READY: begin
-                next_status = x ? FIRST : READY;
-                z = 0;
-            end
-            FIRST: begin
-                next_status = ~x ? SECOND : FIRST;
-                z = 0;
-            end
-            SECOND: begin
-                next_status = x ? FIRST : READY;
-                z = x;
-            end
-        endcase
-    end
 
-    always @(posedge clk) begin
-        if(~aresetn) begin
-            status <= SECOND;
+
+    // Combinational always block for state transition logic. Given the current state and inputs,
+    // what should be next state be?
+    // Combinational always block: Use blocking assignments.
+        always@(*) begin
+                case (state)
+                        S: next = x ? S1 : S;
+                        S1: next = x ? S1 : S10;
+                        S10: next = x ? S1 : S;
+                        default: next = 'x;
+                endcase
         end
-        else begin
-            status <= next_status;
+
+
+
+        // Combinational output logic. I used a combinational always block.
+        // In a Mealy state machine, the output depends on the current state *and*
+        // the inputs.
+        always@(*) begin
+                case (state)
+                        S: z = 0;
+                        S1: z = 0;
+                        S10: z = x;             // This is a Mealy state machine: The output can depend (combinational) on the input.
+                        default: z = 1'bx;
+                endcase
         end
-    end
 
 endmodule
-*/
+
